d14d0c1e4609c525976d04b8eab38100ad835406
Ludovic Stephan <ludovic.stephan@epfl.ch>
2021-12-01 11:27:05 +0100

    Macros !!!

--- 01.jl
+++ 01.jl
@@ -1,9 +1,9 @@
-function find_inc(numbers, gap)
-    return sum(numbers[gap+1:end] .> numbers[1:end-gap])
+function find_inc(numbers::Array{Int, 1}, gap::Int)
+    @views return sum(numbers[gap+1:end] .> numbers[1:end-gap])
 end
 
 open(ARGS[1]) do file
     numbers = parse.(Int, readlines(file))
-    println(find_inc(numbers, 1))
-    println(find_inc(numbers, 3))
+    @info @time find_inc(numbers, 1)
+    @info @time find_inc(numbers, 3)
 end
