cfadbf52d4ebb36b7a145be114d04fe6272ecbe6
Ludovic Stephan <ludovic.stephan@epfl.ch>
2021-12-02 10:24:31 +0100

    Optimization

--- 02.jl
+++ 02.jl
@@ -1,15 +1,15 @@
 function parse_line(line)
     directions = Dict("forward" => 1, "up" => -im, "down" => im,)
     dir, n = split(line)
-    return directions[dir], parse(Int, n)
+    return directions[dir] *  parse(Int, n)
 end
 
 open(ARGS[1]) do file
     pos1 = 0
     pos2 = 0
-    for (dir, n) in map(parse_line, eachline(file))
-        pos1 += dir * n
-        pos2 += n * real(dir) * (1 + imag(pos1) * im)
+    for dir in map(parse_line, eachline(file))
+        pos1 += dir
+        pos2 += real(dir) * (1 + imag(pos1) * im)
     end
 
     println(real(pos1) * imag(pos1))
