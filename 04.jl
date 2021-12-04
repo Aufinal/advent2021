7a97b380a2414df0eaf2d20a7be8e83196a5d6a5
Ludovic Stephan <ludovic.stephan@epfl.ch>
2021-12-04 13:00:00 +0100

    Some optimizations

--- 04.jl
+++ 04.jl
@@ -1,40 +1,37 @@
-function check_bingos(mask)
-    lines = any(all(mask, dims = 2), dims = 1)
-    cols = any(all(mask, dims = 1), dims = 2)
+function check_bingos(marked)
+    lines = any(all(marked, dims = 2), dims = 1)
+    cols = any(all(marked, dims = 1), dims = 2)
 
     return (lines.|cols)[1, 1, :]
 end
 
-score(cards, mask, number, idx) = number * sum(@. cards[:, :, idx] * !mask[:, :, idx])
+score(cards, marked, number, idx) = number * sum(cards[:, :, idx][.!marked[:, :, idx]])
 
 open(ARGS[1]) do file
-    called_numbers = parse.(Int, split(readline(file), ','))
-
+    draws = parse.(Int, split(readline(file), ','))
     parsed_ints = parse.(Int, split(read(file, String)))
-    n_boards = length(parsed_ints) ÷ 25
-    cards = reshape(parsed_ints, 5, 5, n_boards)
-    mask = falses(5, 5, n_boards)
-
+    cards = reshape(parsed_ints, 5, 5, :)
+    marked = falses(size(cards))
     has_won = false
-    bingos = falses(n_boards)
+    bingos = falses(size(cards, 3))
 
-    for number in called_numbers
-        @. mask |= cards == number
+    for number in draws
+        @. marked |= cards == number
         prev_bingos = bingos
-        bingos = check_bingos(mask)
+        bingos = check_bingos(marked)
 
         if !has_won
             idx = findfirst(bingos)
             if !isnothing(idx)
                 has_won = true
-                println(score(cards, mask, number, idx))
+                println(score(cards, marked, number, idx))
             end
         end
 
         if all(bingos)
             new_bingos = prev_bingos .⊻ bingos
             last = findfirst(new_bingos)
-            println(score(cards, mask, number, last))
+            println(score(cards, marked, number, last))
             break
         end
     end
