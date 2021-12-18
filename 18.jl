6eb2e30de7f043a576df80fc83a9b538b583f1ec
Ludovic Stephan <ludovic.stephan@epfl.ch>
2021-12-18 13:35:53 +0100

    Some simplifications

--- 18.jl
+++ 18.jl
@@ -24,21 +24,10 @@ SnailFishNode(left, right) = SnailFishNode(nothing, left, right)
 copy(node::SnailFishLeaf) = SnailFishLeaf(node.value)
 copy(node::SnailFishNode) = SnailFishNode(copy(node.left), copy(node.right))
 
-Base.show(io::IO, node::SnailFishLeaf) = print(io, node.value)
-function Base.show(io::IO, node::SnailFishNode)
-    write(io, "[")
-    print(io, node.left)
-    write(io, ", ")
-    print(io, node.right)
-    write(io, "]")
-end
-
 parse(value::Int, parent::Union{SnailFishNode,Nothing}) = SnailFishLeaf(parent, value)
 parse(expr::Expr, parent::Union{SnailFishNode,Nothing}) =
     SnailFishNode(parent, parse(expr.args[1], nothing), parse(expr.args[2], nothing))
 parse(expr::String) = parse(Meta.parse(expr), nothing)
-
-get_side(node) = node.parent.left == node ? :left : :right
 
 function add_closest(node::SnailFishLeaf, direction::Symbol)
     found = false
@@ -56,26 +45,27 @@ function add_closest(node::SnailFishLeaf, direction::Symbol)
     node.value += value
 end
 
-explode!(node::SnailFish) = explode!(node, 0)
-explode!(node::SnailFishLeaf, depth::Int) = false
-function explode!(node::SnailFishNode, depth::Int)::Bool
+explode!(node::SnailFish) = explode!(node, 0, :root)
+explode!(node::SnailFishLeaf, depth::Int, side::Symbol) = false
+function explode!(node::SnailFishNode, depth::Int, side::Symbol)::Bool
     if depth ≥ 4
         add_closest(node.left, :left)
         add_closest(node.right, :right)
-        setfield!(node.parent, get_side(node), SnailFishLeaf(node.parent, 0))
+        setfield!(node.parent, side, SnailFishLeaf(node.parent, 0))
         return true
     else
-        return explode!(node.left, depth + 1) || explode!(node.right, depth + 1)
+        return explode!(node.left, depth + 1, :left) || explode!(node.right, depth + 1, :right)
     end
 end
 
-split!(node::SnailFishNode) = split!(node.left) || split!(node.right)
-function split!(node::SnailFishLeaf)::Bool
+split!(node::SnailFish) = split!(node, :root)
+split!(node::SnailFishNode, side::Symbol) = split!(node.left, :left) || split!(node.right, :right)
+function split!(node::SnailFishLeaf, side::Symbol)::Bool
     node.value < 10 && return false
     mid = node.value ÷ 2
     setfield!(
         node.parent,
-        get_side(node),
+        side,
         SnailFishNode(node.parent, SnailFishLeaf(mid), SnailFishLeaf(node.value - mid))
     )
     return true
@@ -95,10 +85,8 @@ magnitude(node::SnailFishNode) = 3 * magnitude(node.left) + 2 * magnitude(node.r
 
 open(ARGS[1]) do file
     fishes = parse.(readlines(file))
-    println(magnitude(reduce(+, fishes, init = nothing)))
-    res = 0
-    for i = 1:length(fishes), j = 1:i-1
-        res = max(res, magnitude(fishes[i] + fishes[j]), magnitude(fishes[j] + fishes[i]))
-    end
-    println(res)
+    @time part1 = magnitude(reduce(+, fishes, init = nothing))
+    println(part1)
+    @time part2 = maximum(magnitude(fishes[i] + fishes[j]) for i = 1:length(fishes) for j in 1:length(fishes) if i != j)
+    println(part2)
 end
